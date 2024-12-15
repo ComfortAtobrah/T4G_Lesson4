f38685aaffe29a964b8a1f561ea235b7e728eadf
ComfortAtobrah <atobrahcomfort88@gmail.com>
2024-12-15 14:29:03 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// 
+let go=document.getElementById("ln1");
+go.style.backgroundColor=("blue");
+
+
+let am=document.getElementById("ln2");
+am.style.backgroundColor=("grey");
+am.style.color=("black");
+am.innerHTML=("I know now what the DOM is 🙌");
+
+let is=document.getElementById("ln3");
+is.style.backgroundColor=("red");
+is.style.color=("black");
+is.innerHTML=("I know how to access/ select elements in a DOM🐒");
+
+
+let me=document.getElementById("ln4");
+me.style.backgroundColor=("orange");
+me.style.color=("black");
+me.innerHTML=("I know  how to use the style methods to change the styles of an element in the DOM🐾");
+
+
+let it=document.getElementById("ln5");
+it.style.backgroundColor=("green");
+it.style.color=("black");
+it.innerHTML=("I know how the styling properties differ in JS from CSS☑");
+
+
+let to=document.getElementById("ln6");
+to.style.backgroundColor=("black");
+to.style.color=("white");
+to.innerHTML=("Yes I did it!!! I manipulated the DOM and styled the elements🥰🥰🥰🥰🥰🥰");
